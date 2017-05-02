ec22e1237e8876014c466177aadb18a700097b6a
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-05-02 15:13:28 +0200

    Define abstract base test class, that will mix together the features used the most
    See http://www.scalatest.org/user_guide/defining_base_classes

---
+++ src/test/scala/com/liferay/service/builder/dsl/ServiceBuilderFunSuite.scala
@@ -0,0 +1,9 @@
+package com.liferay.service.builder.dsl
+
+import org.scalatest._
+
+/**
+  * @author Manuel de la Peña
+  */
+abstract class ServiceBuilderFunSuite extends FunSuite
+  with Matchers with OptionValues with Inside with Inspectors
